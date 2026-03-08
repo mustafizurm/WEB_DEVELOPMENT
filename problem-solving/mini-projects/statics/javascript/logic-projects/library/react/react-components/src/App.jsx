79c598864c1fe908bc5ad2b48d5288a5a74033f3
Mustafizur <mustafizurrahman1256@gmail.com>
2026-03-08 13:25:24 +0600

    a lot of folders and file updated

--- problem-solving/mini-projects/statics/javascript/logic-projects/library/react/react-components/src/App.jsx
+++ problem-solving/mini-projects/statics/javascript/logic-projects/library/react/react-components/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import BMICAL from './components/bmi-calculator/BMICAL'
+import TODOBoard from './components/todo-board/TODOBoard'
 
 const App = () => {
   return <>
-     <BMICAL />
+     <TODOBoard />
   </>
 }
 
